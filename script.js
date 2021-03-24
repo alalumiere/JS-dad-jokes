f2dbbacecfd0e8a42d911e25bb7da6e28d9dd6b4
Amanda LaLumiere <amandalalumiere@gmail.com>
2021-03-23 22:35:54 -0500

    complete feature

---
+++ script.js
@@ -0,0 +1,34 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const response = await fetch('https://icanhazdadjoke.com', config);
+
+  const data = await response.json();
+
+  jokeElement.textContent = data.joke;
+}
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       jokeElement.textContent = data.joke;
+//     });
+// }
